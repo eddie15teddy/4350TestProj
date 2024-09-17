946a489cb7b91df7d2eea01114343736ad14335a
Eddie Butkaliuk <eddiebu.1515@gmail.com>
2024-09-16 19:32:21 -0500

    Add numbers using querry string

--- TestProj/TestFunction.cs
+++ TestProj/TestFunction.cs
@@ -18,9 +18,20 @@ namespace MySpace
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string responseString = "I am just a dummy endpoint for COMP 4350. Add num1 and num2 querry params.";
+            string ?num1 = req.Query["num1"];
+            string ?num2 = req.Query["num2"];
+
+            if (!string.IsNullOrEmpty(num1) && !string.IsNullOrEmpty(num2)) 
+            {
+                responseString = $"{num1} + {num2} = {num1 +  num2}";
+            }
+
+
             return new ContentResult
             {
-                Content = "<html><body><h1>Welcome to Azure Functions!</h1></body></html>",
+                Content = $"<html><body><h1>{responseString}</h1></body></html>",
                 ContentType = "text/html",
                 StatusCode = 200
             };
